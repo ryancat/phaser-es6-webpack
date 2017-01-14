bb676cf0874c10e9feec1ed794a90e279efd4a8f
Ryan Chen <ryan.chen@platfora.com>
2017-01-14 09:37:34 -0800

    Playable

--- src/sprites/Baddie.js
+++ src/sprites/Baddie.js
@@ -1,3 +1,5 @@
+import _ from 'lodash'
+
 import Phaser from 'phaser'
 
 export default class extends Phaser.Sprite {
@@ -12,32 +14,53 @@ export default class extends Phaser.Sprite {
 
     game.physics.arcade.enable(this);
     this.body.collideWorldBounds = true;
+    this.body.bounce.x = 1;
+    this.body.bounce.y = 1;
+
+    // init speed
+    const xVelocitySeed = (Math.random() - 0.5) * 2
+    const yVelocitySeed = (Math.random() - 0.5) * 2
+
+    this.currentVelocityX = xVelocitySeed > 0 ? 80 + xVelocitySeed * 120 : -80 + xVelocitySeed * 120
+    this.currentVelocityY = yVelocitySeed > 0 ? 80 + yVelocitySeed * 120 : -80 + yVelocitySeed * 120
+    
+    this.body.velocity.x = this.currentVelocityX;
+    this.body.velocity.y = this.currentVelocityY;
   }
 
   update () {
-    this.body.velocity.x = 0;
-    this.body.velocity.y = 0;
 
-    // Basic move
-    if (this.cursor.left.isDown) {
-      //  Move to the left
-      this.body.velocity.x = -130;
-    }
+
+    // this.body.velocity.x = this.currentVelocityX;
+    // this.body.velocity.y = this.currentVelocityY;
+
+    // if (!this.hittingEdgeX && this.position.x >= game.world.width - this.width / 2) {
+    //   this.currentVelocityX = -this.currentVelocityX;
+    //   this.hittingEdgeX = true;
+    // } else {
+    //   this.hittingEdgeX = false;
+    // }
+
+    // // Basic move
+    // if (this.cursor.left.isDown) {
+    //   //  Move to the left
+    //   this.body.velocity.x = -130;
+    // }
     
-    if (this.cursor.right.isDown) {
-      //  Move to the left
-      this.body.velocity.x = 130;
-    }
+    // if (this.cursor.right.isDown) {
+    //   //  Move to the left
+    //   this.body.velocity.x = 130;
+    // }
     
-    if (this.cursor.up.isDown) {
-      //  Move to the left
-      this.body.velocity.y = -130;
-    }
+    // if (this.cursor.up.isDown) {
+    //   //  Move to the left
+    //   this.body.velocity.y = -130;
+    // }
     
-    if (this.cursor.down.isDown) {
-      //  Move to the left
-      this.body.velocity.y = 130;
-    }
+    // if (this.cursor.down.isDown) {
+    //   //  Move to the left
+    //   this.body.velocity.y = 130;
+    // }
 
   }
 
--- src/states/Game.js
+++ src/states/Game.js
@@ -1,6 +1,7 @@
 /* globals __DEV__ */
 import Phaser from 'phaser'
 import Ninja from '../sprites/Ninja'
+import Baddie from '../sprites/Baddie'
 
 export default class extends Phaser.State {
   init () {}
@@ -16,6 +17,71 @@ export default class extends Phaser.State {
     // banner.smoothed = false
     // banner.anchor.setTo(0.5)
 
+    this.createScoreText()
+    this.createHighScoreText()
+    this.countSec()
+    this.createNinja()
+
+    // Add baddie
+    this.baddies = this.game.add.group()
+
+    for (let i = 0; i < 30; i++) {
+      this.baddies.add(new Baddie({
+        game: this,
+        x: this.world.centerX + Math.random() * this.world.centerX,
+        y: this.world.centerY + Math.random() * this.world.centerY,
+        asset: 'baddie'
+      }))
+    }
+  }
+
+  update () {
+    this.game.physics.arcade.overlap(this.ninja, this.baddies, this.collideNinjaBaddies, null, this)
+
+
+  }
+
+  render () {
+    if (__DEV__) {
+      this.game.debug.spriteInfo(this.ninja, 32, 32)
+    }
+  }
+
+  createScoreText () {
+    this.secCount = 0
+    this.scoreText = this.game.add.text(16, this.game.world.height - 40, 'score: ' + this.secCount + 's', { fontSize: '32px', fill: '#FFF' })
+  }
+
+  countSec () {
+    this.scoreText.text = 'score: ' + this.secCount++ + 's'
+    this.countSecTimout = setTimeout(this.countSec.bind(this), 1000)
+  }
+
+  stopCount () {
+    clearTimeout(this.countSecTimout)
+  }
+
+  createHighScoreText () {
+    this.highScores = [0]
+    this.highScoreText = this.game.add.text(this.game.world.width - 50, this.game.world.height - 100, this.getHighScoreText(), { fontSize: '16px', fill: '#FFF' })
+  }
+
+  recordHighscore () {
+    this.highScores.push(this.secCount)
+    this.highScores = this.highScores.sort().reverse()
+    if (this.highScores.length > 3) {
+      this.highScores.pop()
+    }
+    this.highScoreText.text = this.getHighScoreText()
+  }
+
+  getHighScoreText () {
+    return this.highScores.map((score) => {
+      return score + 's'
+    }).join('\n')
+  }
+
+  createNinja () {
     this.ninja = new Ninja({
       game: this,
       x: this.world.centerX,
@@ -26,9 +92,35 @@ export default class extends Phaser.State {
     this.game.add.existing(this.ninja)
   }
 
-  render () {
-    if (__DEV__) {
-      this.game.debug.spriteInfo(this.ninja, 32, 32)
+  collideNinjaBaddies (ninja, baddie) {
+    this.killNinja(ninja, baddie)
+  }
+
+  killNinja (ninja, baddie) {
+    let that = this,
+        countDown = 3
+    
+    this.stopCount()
+    this.recordHighscore()
+    ninja.kill()
+    this.ninjaRespawn(countDown)
+
+    setTimeout(function () {
+      that.createNinja()
+      that.secCount = 0
+      that.countSec()
+    }, 1000 * countDown)
+
+  }
+
+  ninjaRespawn (countDown) {
+    const countdownfn = () => {
+      if (countDown >= 0) {
+        this.scoreText.text = 'Respawn: ' + countDown-- + 's' 
+        setTimeout(countdownfn, 1000)
+      }
     }
+
+    countdownfn()
   }
 }
