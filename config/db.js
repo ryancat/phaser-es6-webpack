82dbc236799f37cadf35e75fb57b38299cd15911
Ryan Chen <ryan.chen@platfora.com>
2017-01-30 23:49:23 -0800

    Add server v1

--- config/db.js
+++ config/db.js
@@ -4,5 +4,5 @@ const devUrl = 'mongodb://localhost/dotNinja',
       prodUrl = 'mongodb://heroku_jfpphlpm:djp1pl55ueo1c3m9giofenjsvi@ds137759.mlab.com:37759/heroku_jfpphlpm'
 
 module.exports = {
-  url: __DEV__ ? devUrl : prodUrl
+  url: prodUrl
 };
